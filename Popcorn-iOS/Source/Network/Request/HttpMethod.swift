1f7af833fba84726836217ef11de4c9338d23550
snughnu <ksnughnu@gmail.com>
2024-11-11 22:13:14 +0900

    feat: HttpMethod 열거형 구현

---
+++ Popcorn-iOS/Source/Network/Request/HttpMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HttpMethod.swift
+//  Popcorn-iOS
+//
+//  Created by 김성훈 on 11/11/24.
+//
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
