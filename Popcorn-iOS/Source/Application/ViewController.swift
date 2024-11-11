54037291425812e95ccccf5aaee3b6f79aa37ff7
MinwooJe <gpatration77@gmail.com>
2024-11-11 16:13:47 +0900

    Initial Commit
    - 폴더링
    - 스토리보드 제거
    - 린트 적용

---
+++ Popcorn-iOS/Source/Application/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 11/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
