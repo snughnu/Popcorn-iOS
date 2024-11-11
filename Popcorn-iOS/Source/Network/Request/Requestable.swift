78fc79810d4f996309adb68dbea0547adb88f7dc
snughnu <ksnughnu@gmail.com>
2024-11-11 22:18:04 +0900

    feat: Requestable 구현

---
+++ Popcorn-iOS/Source/Network/Request/Requestable.swift
@@ -0,0 +1,29 @@
+//
+//  Requestable.swift
+//  Popcorn-iOS
+//
+//  Created by 김성훈 on 11/11/24.
+//
+
+import Foundation
+
+protocol Requestable {
+    var baseURL: String { get }
+    var httpMethod: HttpMethod { get }
+    var path: String { get }
+    var queryItems: [URLQueryItem] { get }
+    var headers: [String: String] { get }
+    var bodyParameters: Encodable { get }
+    
+    func makeURLRequest() -> URLRequest?
+}
+
+extension Requestable {
+    func makeURL() -> URL? {
+        guard var components = URLComponents(string: baseURL) else { return nil }
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components.url
+    }
+}
