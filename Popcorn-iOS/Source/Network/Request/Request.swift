33b8c6429ee7de8ecc940a66296055accaf8e958
snughnu <ksnughnu@gmail.com>
2024-11-11 22:34:50 +0900

    feat: Request 구현

---
+++ Popcorn-iOS/Source/Network/Request/Request.swift
@@ -0,0 +1,43 @@
+//
+//  Request.swift
+//  Popcorn-iOS
+//
+//  Created by 김성훈 on 11/11/24.
+//
+
+import Foundation
+
+final class Request: Requestable {
+    var baseURL: String
+    var httpMethod: HttpMethod
+    var path: String
+    var queryItems: [URLQueryItem]
+    var headers: [String : String]
+    var bodyParameters: Encodable
+    
+    init(
+        baseURL: String = "",
+        httpMethod: HttpMethod,
+        path: String,
+        queryItems: [URLQueryItem] = [],
+        headers: [String : String] = [:],
+        bodyParameters: Encodable
+    ) {
+        self.baseURL = baseURL
+        self.httpMethod = httpMethod
+        self.path = path
+        self.queryItems = queryItems
+        self.headers = headers
+        self.bodyParameters = bodyParameters
+    }
+    
+    func makeURLRequest() -> URLRequest? {
+        guard let url = makeURL() else { return nil }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.allHTTPHeaderFields = headers
+        urlRequest.httpMethod = httpMethod.rawValue
+        
+        return urlRequest
+    }
+}
