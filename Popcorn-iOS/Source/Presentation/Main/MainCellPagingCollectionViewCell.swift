c145ad022a5c9e1089abe24c45b205b11f42462e
MinwooJe <gpatration77@gmail.com>
2024-11-16 16:04:57 +0900

    feat: MainCellPagingCollectionViewCell 구현

---
+++ Popcorn-iOS/Source/Presentation/Main/MainCellPagingCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  MainCellPagingCollectionViewCell.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 11/16/24.
+//
+
+import UIKit
+
+final class MainCellPagingCollectionViewCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSubviews()
+        configureLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Configure UI
+extension MainCellPagingCollectionViewCell {
+    private func configureSubviews() {
+        [imageView].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            imageView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            imageView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+}
