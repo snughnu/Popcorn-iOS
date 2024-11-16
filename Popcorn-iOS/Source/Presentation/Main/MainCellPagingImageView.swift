1c6065a5f92b570dffc90f25e0f8fa24c48bc5fa
MinwooJe <gpatration77@gmail.com>
2024-11-16 15:20:21 +0900

    feat: UICollectionView DataSource 메서드 구현

--- Popcorn-iOS/Source/Presentation/Main/MainCellPagingImageView.swift
+++ Popcorn-iOS/Source/Presentation/Main/MainCellPagingImageView.swift
@@ -19,12 +19,37 @@ final class MainCellPagingImageView: UIView {
 
     init() {
         super.init(frame: .zero)
+        configureInitialSetting()
         configureSubviews()
         configureLayout()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Configure Initial Setting
+extension MainCellPagingImageView {
+    private func configureInitialSetting() {
+        pagingImageCollectionView.dataSource = self
+    }
+}
+
+// MARK: - Configure CollectionView DataSource
+extension MainCellPagingImageView: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 10
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
 
