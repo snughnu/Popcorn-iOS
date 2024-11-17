07875e5a1246850523cc82852878e55b437eab55
MinwooJe <gpatration77@gmail.com>
2024-11-17 15:24:05 +0900

    feat: 메인화면의 팝업 미리보기 관련 엔티티, 관심사별 팝업 엔티티 정의, 관심사 열거형 정의

---
+++ Popcorn-iOS/Source/Domain/Model/Entities/PopupPreview.swift
@@ -0,0 +1,15 @@
+//
+//  PopupPreview.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 11/17/24.
+//
+
+import Foundation
+
+struct PopupPreview {
+    let popUpImage: Data
+    let popUpTitle: String
+    let popUpDueDate: Date
+    let isPick: Bool
+}
