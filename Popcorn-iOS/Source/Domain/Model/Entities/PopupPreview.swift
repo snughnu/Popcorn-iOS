957a1d205b4bbcd99943b175a08d93be21258ada
MinwooJe <gpatration77@gmail.com>
2024-11-21 19:06:22 +0900

    refactor: `PopupPreviewRepresentable` `ClosingSoonPopupPreview` 삭제 후 `PopupPreview`로 통합
    기존에는 `ClosingSoonPopupPreview` , `PopupPreview` 엔티티를 따로 두고,  `PopupPreviewRepresentable`를 이용하여 다형성을 유지하려고 하였음.
    
    그러나 프로토콜 구현을 위해 `ClosingSoonPopupPreview` 에서만 사용되는 `popupStartDate`, `popupLocation` 에 기본값 `nil`을 할당하였고, 이를 `ClosingSoonPopupPreview`에서 재정의 하지 못하였음.
    
    (프로토콜에서는 var와 옵셔널로 선언되었으나, 구조체에서는 let과 non-optional로 선언했기 때문)
    
    따라서 MainSceneViewModel의 `preparePopupPreview` 옵셔널 바인딩이 실패하여, 셀에 올바른 데이터를 할당하지 못하였음.
    
    따라서 `PopupPreview` 구조체에 옵셔널 타입의 `popupStartDate`, `popupLocation` 프로퍼티를 선언 및 생성자에 기본 값을 할당하는 방식으로 변경함.

--- Popcorn-iOS/Source/Domain/Model/Entities/PopupPreview.swift
+++ Popcorn-iOS/Source/Domain/Model/Entities/PopupPreview.swift
@@ -7,9 +7,28 @@
 
 import Foundation
 
-struct PopupPreview: PopupPreviewRepresentable {
+struct PopupPreview {
     let popupImage: Data
     let popupTitle: String
     let popupDueDate: Date
     let isPick: Bool
+
+    let popupStartDate: Date?
+    let popupLocation: String?
+
+    init(
+        popupImage: Data,
+        popupTitle: String,
+        popupDueDate: Date,
+        isPick: Bool,
+        popupStartDate: Date? = nil,
+        popupLocation: String? = nil
+    ) {
+        self.popupImage = popupImage
+        self.popupTitle = popupTitle
+        self.popupDueDate = popupDueDate
+        self.isPick = isPick
+        self.popupStartDate = popupStartDate
+        self.popupLocation = popupLocation
+    }
 }
