4fec2b24659df40f38186da94ea767363f7083f7
MinwooJe <gpatration77@gmail.com>
2024-11-24 19:26:26 +0900

    refactor: `applyCustomFont`가 NSMutableAttributedString을 반환하도록 변경
    UILabel, UIButton.title 등 다양한 컴포넌트에 적용시키기 위함

--- Popcorn-iOS/Source/Util/Extensions/UILabel+.swift
+++ Popcorn-iOS/Source/Util/Extensions/UILabel+.swift
@@ -11,22 +11,26 @@ extension UILabel {
     static let letterSpacing: CGFloat = 0
     static let smallLetterSpacing: CGFloat = -1
 
-    private func applyCustomFont(name: String, size: CGFloat, text: String, isSmallLetterSpacing: Bool = false) {
-        let font = UIFont(name: name, size: size) ?? UIFont.systemFont(ofSize: size)
-        let attributedString = NSMutableAttributedString(string: text)
-        let letterSpacing = isSmallLetterSpacing ? UILabel.smallLetterSpacing : UILabel.letterSpacing
+    static func applyCustomFont(
+        text: String,
+        fontName: String,
+        fontSize: CGFloat,
+        isSmallLetterSpacing: Bool = false
+    ) -> NSAttributedString {
+        let font = UIFont(name: fontName, size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
+        let letterSpacing = isSmallLetterSpacing ? smallLetterSpacing : letterSpacing
 
+        let attributedString = NSMutableAttributedString(string: text)
         attributedString.addAttributes(
             [.font: font, .kern: letterSpacing],
             range: NSRange(location: 0, length: attributedString.length)
         )
-        self.attributedText = attributedString
+        return attributedString
     }
 
     func popcornMedium(text: String, size: CGFloat) {
-        self.applyCustomFont(name: "RobotoFlex-Regular_Medium", size: size, text: text)
+        self.attributedText = UILabel.applyCustomFont(text: text, fontName: RobotoFontName.robotoMedium, fontSize: size)
     }
-    
     
     /// UILabel의 RobotoFlex-SemiBold Font를 적용시키는 함수.
     /// - Parameters:
@@ -34,10 +38,10 @@ extension UILabel {
     ///   - size: 폰트 크기
     ///   - isSmallLetterSpacing: 아이디/비밀번호 찾기 화면의 네비게이션 타이틀에서 자간이 -1이므로 true값을 주면, -1 자간 적용됨.
     func popcornSemiBold(text: String, size: CGFloat, isSmallLetterSpacing: Bool = false) {
-        self.applyCustomFont(
-            name: "RobotoFlex-Regular_SemiBold",
-            size: size,
+        self.attributedText = UILabel.applyCustomFont(
             text: text,
+            fontName: RobotoFontName.robotoSemiBold,
+            fontSize: size,
             isSmallLetterSpacing: isSmallLetterSpacing
         )
     }
