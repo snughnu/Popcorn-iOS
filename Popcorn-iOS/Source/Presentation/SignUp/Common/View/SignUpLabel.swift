cfe7cc91a67187f56f809f343d9dbb8e1d1cdfa6
snughnu <ksnughnu@gmail.com>
2024-11-24 17:52:48 +0900

    feat: 회원가입화면 공통 레이블 코드 구현

---
+++ Popcorn-iOS/Source/Presentation/SignUp/Common/View/SignUpLabel.swift
@@ -0,0 +1,41 @@
+//
+//  SignUpLabel.swift
+//  Popcorn-iOS
+//
+//  Created by 김성훈 on 11/24/24.
+//
+
+import UIKit
+
+final class SignUpLabel: UILabel {
+    private let leftPadding: CGFloat = 20
+
+    init(text: String? = nil) {
+        super.init(frame: .zero)
+        self.text = text
+        configureLabel()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func drawText(in rect: CGRect) {
+        let insetRect = rect.inset(by: UIEdgeInsets(top: 0, left: leftPadding, bottom: 0, right: 0))
+        super.drawText(in: insetRect)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftPadding, height: size.height)
+    }
+}
+
+// MARK: - Configure Label
+extension SignUpLabel {
+    private func configureLabel() {
+        font = UIFont(name: "Roboto-Medium", size: 15)
+        textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        textAlignment = .left
+    }
+}
