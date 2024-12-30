a40c809b379c5972836bec3d943f4f8945326b0a
MinwooJe <gpatration77@gmail.com>
2024-12-30 12:19:08 +0900

    refactor: MainDetailViewController에서 PopupTitleCollectionViewCell 관련 코드 이동

--- Popcorn-iOS/Source/Presentation/Main/PopupDetailView/PopupDetailInfoView.swift
+++ Popcorn-iOS/Source/Presentation/Main/PopupDetailView/PopupDetailInfoView.swift
@@ -216,39 +216,3 @@ extension PopupDetailInfoView {
         ])
     }
 }
-
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
-struct UIViewPreview<View: UIView>: UIViewRepresentable {
-    let view: View
-
-    init(_ builder: @escaping () -> View) {
-        view = builder()
-    }
-
-    // MARK: - UIViewRepresentable
-
-    func makeUIView(context: Context) -> UIView {
-        return view
-    }
-
-    func updateUIView(_ view: UIView, context: Context) {
-        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
-    }
-}
-
-#endif
-
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
-
-struct Preview: PreviewProvider {
-    static var previews: some View {
-        UIViewPreview {
-            let myView = PopupDetailInfoView()
-            return myView
-        }.previewLayout(.sizeThatFits)
-    }
-}
-#endif
