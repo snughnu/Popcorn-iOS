bd2cfd39904f4d71f3420309215976df5e6e41f7
snughnu <ksnughnu@gmail.com>
2025-01-15 20:54:59 +0900

    feat: Token구조체와 LoginResponse구조체 분리

--- Popcorn-iOS/Source/Data/Token/Token.swift
+++ Popcorn-iOS/Source/Data/Token/Token.swift
@@ -10,4 +10,13 @@ import Foundation
 struct Token: Decodable {
     let accessToken: String
     let refreshToken: String
+    let accessExpiredAt: String
+    let refreshExpiredAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case refreshToken = "refresh_token"
+        case accessExpiredAt = "access_expired_at"
+        case refreshExpiredAt = "refresh_expired_at"
+    }
 }
