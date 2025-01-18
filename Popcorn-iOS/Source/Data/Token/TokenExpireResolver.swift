833768055ffcb28de87a0d900f52335592efd541
snughnu <ksnughnu@gmail.com>
2025-01-18 17:04:16 +0900

    feat: 토큰의 타입 저장/가져오기 로직 추가
    - 토큰의 타입에 따라 access토큰을 재발급하는 로직을 분리하였습니다.

--- Popcorn-iOS/Source/Data/Token/TokenExpireResolver.swift
+++ Popcorn-iOS/Source/Data/Token/TokenExpireResolver.swift
@@ -6,11 +6,14 @@
 //
 
 import Foundation
+import KakaoSDKAuth
 
 final class TokenExpireResolver {
     private let tokenRepository = TokenRepository()
 
     func handleTokenExpiration(completion: @escaping (Bool) -> Void) {
+        let loginType = tokenRepository.fetchLoginType()
+
         if let accessToken = tokenRepository.fetchAccessToken() {
             print("Access Token 유효: \(accessToken)")
             completion(true)
@@ -18,15 +21,34 @@ final class TokenExpireResolver {
         }
 
         if let refreshToken = tokenRepository.fetchRefreshToken() {
-            tokenRepository.reissueAccessToken(refreshToken: refreshToken) { result in
-                switch result {
-                case .success(let newToken):
-                    print("새로운 Access Token 발급 성공")
-                    self.tokenRepository.saveToken(with: newToken)
-                    completion(true)
-                case .failure:
-                    print("Access Token 재발급 실패")
-                    completion(false)
+            if loginType == "kakao" {
+                AuthApi.shared.refreshToken { [weak self] oauthToken, error in
+                    if let error = error {
+                        print("카카오 Access Token 재발급 실패: \(error.localizedDescription)")
+                        completion(false)
+                    } else if let oauthToken = oauthToken {
+                        print("카카오 Access Token 재발급 성공")
+                        let newToken = Token(
+                            accessToken: oauthToken.accessToken,
+                            refreshToken: oauthToken.refreshToken,
+                            accessExpiredAt: ISO8601DateFormatter().string(from: oauthToken.expiredAt),
+                            refreshExpiredAt: ISO8601DateFormatter().string(from: oauthToken.refreshTokenExpiredAt)
+                        )
+                        self?.tokenRepository.saveToken(with: newToken)
+                        completion(true)
+                    }
+                }
+            } else {
+                tokenRepository.reissueAccessToken(refreshToken: refreshToken) { result in
+                    switch result {
+                    case .success(let newToken):
+                        print("새로운 Access Token 발급 성공")
+                        self.tokenRepository.saveToken(with: newToken)
+                        completion(true)
+                    case .failure:
+                        print("Access Token 재발급 실패")
+                        completion(false)
+                    }
                 }
             }
         } else {
