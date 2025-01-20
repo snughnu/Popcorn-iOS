ce27e7bff703fd5f28df60ba4c53df2b89c6c887
MinwooJe <gpatration77@gmail.com>
2025-01-20 16:27:16 +0900

    feat: WriteReviewViewModel 구현

---
+++ Popcorn-iOS/Source/Presentation/Main/ViewModel/WriteReviewViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  WriteReviewViewModel.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 1/20/25.
+//
+
+import UIKit
+
+final class WriteReviewViewModel {
+    let reviewTextViewPlaceHolderText = "팝업스토어 리뷰를 남겨주세요."
+
+    private var rating: Float = 0 {
+        didSet {
+            validateSubmitEnabled()
+        }
+    }
+
+    private var reviewText: String = "" {
+        didSet {
+            validateSubmitEnabled(text: reviewText)
+        }
+    }
+
+    private var reviewImages: [UIImage] = [] {
+        didSet {
+            validateSubmitEnabled()
+            reviewImagesPublisher?()
+        }
+    }
+
+    private var isSubmitEnabled: Bool = false {
+        didSet {
+            isSubmitEnabledPublisher?(isSubmitEnabled)
+        }
+    }
+
+    // MARK: - Output
+    var isSubmitEnabledPublisher: ((_ isSubmitEnabled: Bool) -> Void)?
+    var reviewImagesPublisher: (() -> Void)?
+
+    private func validateSubmitEnabled(text: String = "") {
+        let isRatingValid = rating > 0 && rating <= 5
+        let isTextValid = text != reviewTextViewPlaceHolderText && (reviewText.count > 10)
+        let isImageValid = reviewImages.count <= 10
+
+        isSubmitEnabled = isRatingValid && isTextValid && isImageValid
+    }
+}
+
+// MARK: - Public Interface
+extension WriteReviewViewModel {
+    func provideReviewImages(at index: Int) -> UIImage {
+        guard index < reviewImages.count else { return UIImage(resource: .uploadImagePlaceholder) }
+        return reviewImages[index]
+    }
+}
+
+// MARK: - Input
+extension WriteReviewViewModel {
+    func updateRating(_ rating: Float) {
+        self.rating = rating
+    }
+
+    func updateReviewText(_ text: String) {
+        self.reviewText = text
+    }
+
+    func addImage(_ image: UIImage) {
+        reviewImages.append(image)
+    }
+}
+
+// MARK: - Network
+extension WriteReviewViewModel {
+    func postReviewData() {
+    }
+}
