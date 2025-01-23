efce3942017658abb96de7f2306c6ba62c479d48
MinwooJe <gpatration77@gmail.com>
2025-01-23 15:19:10 +0900

    refactor: 네트워크 에러 수정

--- Popcorn-iOS/Source/Data/Network/NetworkManager/NetworkError.swift
+++ Popcorn-iOS/Source/Data/Network/NetworkManager/NetworkError.swift
@@ -7,39 +7,39 @@
 
 import Foundation
 
-enum NetworkError: LocalizedError {
-    case unknown
-    case components
-    case urlRequest
-    case server(ServerError)
-    case emptyData
-    case parsing
-    case decoding(Error)
+enum NetworkError: Error {
+    case invalidURL                       // 유효하지 않은 URL
+    case responseError                    // 유효하지 않은 응답값일 경우
+    case decodingError(Error)             // 데이터 파싱 실패
+    case emptyData                        // 응답 데이터가 비어있는 경우
+    case serverError(ServerError)         // 서버 에러
+    case requestFailed(String)            // 서버 요청 실패한 경우
+    case unknown                          // 알 수 없는 오류
 
-    var errorDescription: String? {
+    var description: String {
         switch self {
-        case .unknown:
-            return "Unknown error"
-        case .components:
-            return "Invalid URL components"
-        case .urlRequest:
-            return "Invalid URL request"
-        case .server(let serverError):
-            return "Server Error: \(serverError)"
+        case .invalidURL:
+            "URL이 올바르지 않습니다."
+        case .responseError:
+            "응답값이 유효하지 않습니다."
+        case .decodingError(let description):
+            "디코딩 에러: \(description)"
         case .emptyData:
-            return "Empty data"
-        case .parsing:
-            return "Failed to parse data"
-        case .decoding(let error):
-            return "Error While Decoding: \(error)"
+            "데이터가 없습니다."
+        case .serverError(let code):
+            "서버 에러 \(code.rawValue)"
+        case .requestFailed(let message):
+            "서버 요청 실패 \(message)"
+        case .unknown:
+            "알 수 없는 에러"
         }
     }
 }
 
 enum ServerError: Int {
-    case unknown
-    case badRequest = 400
-    case unauthorized = 401
-    case forbidden = 403
-    case notFound = 404
+    case unknown = -1           // 알 수 없는 오류
+    case badRequest = 400       // 잘못된 요청
+    case unauthorized = 401     // 인증 실패
+    case forbidden = 403        // 접근 권한 없음
+    case notFound = 404         // 요청한 리소스를 찾을 수 없음
 }
