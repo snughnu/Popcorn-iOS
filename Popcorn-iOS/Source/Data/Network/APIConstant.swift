f6282bf98806671df3ee726060a79ea486edca20
MinwooJe <gpatration77@gmail.com>
2025-01-23 15:26:49 +0900

    feat: API 경로 상수화

---
+++ Popcorn-iOS/Source/Data/Network/APIConstant.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstant.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 1/23/25.
+//
+
+struct APIConstant {
+    static let baseURL = "https://popcorm.store"
+    static let authPath = "/auth"
+    static let popupPath = "/popups"
+}
