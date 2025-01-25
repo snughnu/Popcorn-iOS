710fe62a54e49262682963cf45d575bfbd8109f5
snughnu <ksnughnu@gmail.com>
2025-01-25 16:38:59 +0900

    refactor: 로그인실패시 에러처리에 대한 로직 수정

--- Popcorn-iOS/Source/Presentation/Login/LoginScene/Models/LoginResponse.swift
+++ Popcorn-iOS/Source/Presentation/Login/LoginScene/Models/LoginResponse.swift
@@ -10,27 +10,5 @@ import Foundation
 struct LoginResponse: Decodable {
     let resultCode: Int
     let status: String
-    let data: LoginResponseData
-}
-
-enum LoginResponseData: Decodable {
-    case token(Token)
-    case errorMessage(String)
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if let token = try? container.decode(Token.self) {
-            self = .token(token)
-        } else if let errorMessage = try? container.decode(String.self) {
-            self = .errorMessage(errorMessage)
-        } else {
-            throw DecodingError.typeMismatch(
-                LoginResponseData.self,
-                DecodingError.Context(
-                    codingPath: decoder.codingPath,
-                    debugDescription: "Expected Token or String in data field"
-                )
-            )
-        }
-    }
+    let data: Token
 }
