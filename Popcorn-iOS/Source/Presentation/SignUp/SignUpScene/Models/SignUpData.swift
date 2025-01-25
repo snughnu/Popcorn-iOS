d39af34af8713a74e552481eb348bdc3b835ed1d
snughnu <ksnughnu@gmail.com>
2025-01-25 15:23:17 +0900

    refactor: SignUpData 구조체 분리

--- Popcorn-iOS/Source/Presentation/SignUp/SignUpScene/Models/SignUpData.swift
+++ Popcorn-iOS/Source/Presentation/SignUp/SignUpScene/Models/SignUpData.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 struct SignUpData: Codable {
-    struct FirstSignupDto: Codable {
-        let name: String
-        let username: String
-        let password: String
-        let email: String
-    }
-
-    struct SecondSignupDto: Codable {
-        let nickname: String
-        let profileId: Int
-        let interests: [String]
-    }
-
     let firstSignupDto: FirstSignupDto
     var secondSignupDto: SecondSignupDto?
+}
+
+struct FirstSignupDto: Codable {
+    let name: String
+    let username: String
+    let password: String
+    let email: String
+}
+
+struct SecondSignupDto: Codable {
+    let nickname: String
+    let profileId: Int
+    let interests: [String]
 }
