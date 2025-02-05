0eb70542b46c9b1e3162e586fd160b9c95d96147
MinwooJe <gpatration77@gmail.com>
2025-02-05 16:43:34 +0900

    feat: ImageFetchUseCase 구현

---
+++ Popcorn-iOS/Source/Domain/UseCases/ImageFetchUseCase/Implementations/ImageFetchUseCase.swift
@@ -0,0 +1,19 @@
+//
+//  ImageFetchUseCase.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 2/5/25.
+//
+import Foundation
+
+final class ImageFetchUseCase: ImageFetchUseCaseProtocol {
+    let repository: ImageFetchManagerRepsitoryProtocol
+
+    init(repository: ImageFetchManagerRepsitoryProtocol) {
+        self.repository = repository
+    }
+
+    func fetchImage(url: URL, completion: @escaping (Result<Data, ImageFetchError>) -> Void) {
+        repository.fetchImage(url: url, completion: completion)
+    }
+}
