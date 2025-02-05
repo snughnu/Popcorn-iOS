f382999b3890374c7dd5d3c42a80e9a6f4cdbe8a
MinwooJe <gpatration77@gmail.com>
2025-02-05 16:37:31 +0900

    feat: ImageFetchMangerRepositoryProtocol 구현

---
+++ Popcorn-iOS/Source/Domain/Interfaces/Repositories/ImageFetchManager/ImageFetchManagerRepsitoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ImageFetchManagerRepsitoryProtocol.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 2/5/25.
+//
+
+import Foundation
+
+protocol ImageFetchManagerRepsitoryProtocol {
+    func fetchImage(url: URL, completion: @escaping (Result<Data, ImageFetchError>) -> Void)
+}
