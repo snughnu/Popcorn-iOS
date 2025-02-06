c5ed4d347864c59f62c7e8e6d37de0cb052e8e68
MinwooJe <gpatration77@gmail.com>
2025-02-06 17:31:57 +0900

    feat: PopupPreviewResponseDTO 엔티티 변환 로직 구현

--- Popcorn-iOS/Source/Data/DTO/MainScene/PopupPreviewResponseDTO.swift
+++ Popcorn-iOS/Source/Data/DTO/MainScene/PopupPreviewResponseDTO.swift
@@ -5,6 +5,8 @@
 //  Created by 제민우 on 2/6/25.
 //
 
+import Foundation
+
 struct PopupPreviewResponseDTO: Decodable {
     let popupId: Int
     let title: String
@@ -12,4 +14,21 @@ struct PopupPreviewResponseDTO: Decodable {
     let startDate: String
     let endDate: String
     let address: String
+}
+
+extension PopupPreviewResponseDTO {
+    func toEntity() -> PopupPreview {
+        let errorDate = DateFormatter.apiDateFormatter.date(from: "1900-01-01 00:00:00")!
+        let endDate = DateFormatter.apiDateFormatter.date(from: endDate) ?? errorDate
+        let startDate = DateFormatter.apiDateFormatter.date(from: startDate) ?? errorDate
+
+        return PopupPreview(
+            popupId: popupId,
+            popupImageUrl: imageUrl,
+            popupTitle: title,
+            popupEndDate: endDate,
+            popupStartDate: startDate,
+            popupLocation: address
+        )
+    }
 }
