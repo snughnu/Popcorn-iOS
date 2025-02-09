8bf203a04fbc502349e61e38803ce5f107585aa7
snughnu <ksnughnu@gmail.com>
2025-02-10 00:02:54 +0900

    refactor: 회원가입화면1의 뷰모델에서 유즈케이스와 레포지토리 로직 분리

--- Popcorn-iOS/Source/Domain/UseCases/SignUpUseCase/SignUpUseCase.swift
+++ Popcorn-iOS/Source/Domain/UseCases/SignUpUseCase/SignUpUseCase.swift
@@ -16,6 +16,14 @@ protocol SignUpUseCaseProtocol {
         completion: @escaping (Result<Bool, Error>) -> Void
     )
     func executeSendSignUpData(signupData: SignUpRequestDTO, completion: @escaping (Result<Bool, Error>) -> Void)
+
+    func isNameFormatted(_ name: String) -> Bool
+    func isIdFormatted(_ id: String) -> Bool
+    func isPwFormatted(_ password: String) -> Bool
+    func isConfirmPwFormatted(_ password: String, _ confirmPassword: String) -> Bool
+    func isEmailFormatted(_ email: String) -> Bool
+
+    func saveSignUpData(name: String, id: String, password: String, email: String) -> Bool
 }
 
 class SignUpUseCase: SignUpUseCaseProtocol {
@@ -92,5 +100,46 @@ extension SignUpUseCase {
                 completion(.failure(error))
             }
         }
+    }
+
+    func saveSignUpData(name: String, id: String, password: String, email: String) -> Bool {
+        let data = SignUpRequestDTO(
+            firstSignupDto: FirstSignupDto(name: name, username: id, password: password, email: email),
+            secondSignupDto: nil
+        )
+        return signUpRepository.saveSignUpData(signUpData: data)
+    }
+}
+
+// MARK: - 정규식
+extension SignUpUseCase {
+    func isNameFormatted(_ name: String) -> Bool {
+        let nameRegex = "^[가-힣a-zA-Z]{2,10}$"
+        let nameTest = NSPredicate(format: "SELF MATCHES %@", nameRegex)
+        return nameTest.evaluate(with: name)
+    }
+
+    func isIdFormatted(_ id: String) -> Bool {
+        let idRegex = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{6,12}$"
+        let idTest = NSPredicate(format: "SELF MATCHES %@", idRegex)
+        return idTest.evaluate(with: id)
+    }
+
+    func isPwFormatted(_ password: String) -> Bool {
+        let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,16}$"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        return passwordTest.evaluate(with: password)
+    }
+
+    func isConfirmPwFormatted(_ password: String, _ confirmPassword: String) -> Bool {
+        let pw = password
+        let confirmPw = confirmPassword
+        return (pw == confirmPw)
+    }
+
+    func isEmailFormatted(_ email: String) -> Bool {
+        let emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailTest.evaluate(with: email)
     }
 }
