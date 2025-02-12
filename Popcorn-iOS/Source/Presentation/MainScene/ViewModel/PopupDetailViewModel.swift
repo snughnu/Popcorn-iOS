67806b7837e5d900af39c4194fd3a2fe14544cd2
MinwooJe <gpatration77@gmail.com>
2025-02-13 00:54:46 +0900

    refactor: 상세화면 관련 ViewModel 생성자 수정

--- Popcorn-iOS/Source/Presentation/MainScene/ViewModel/PopupDetailViewModel.swift
+++ Popcorn-iOS/Source/Presentation/MainScene/ViewModel/PopupDetailViewModel.swift
@@ -7,72 +7,13 @@
 
 import Foundation
 
-struct PopupMainInformationViewData {
-    let popupTitle: String
-    let popupPeriod: String
-    let isUserPick: Bool
-    let hashTags: [String]
-
-    init(
-        popupTitle: String = "",
-        popupPeriod: String = "",
-        isUserPick: Bool = false,
-        hashTags: [String] = []
-    ) {
-        self.popupTitle = popupTitle
-        self.popupPeriod = popupPeriod
-        self.isUserPick = isUserPick
-        self.hashTags = hashTags
-    }
-}
-
-struct PopupDetailInformationViewData {
-    let address: String
-    let officialLink: String
-    let buisinessHours: String
-    let introduce: String
-
-    init(
-        address: String = "",
-        officialLink: String = "",
-        buisinessHours: String = "",
-        introduce: String = ""
-    ) {
-        self.address = address
-        self.officialLink = officialLink
-        self.buisinessHours = buisinessHours
-        self.introduce = introduce
-    }
-}
-
-struct PopupRatingViewData {
-    let totalRatingCount: Int
-    let averageRating: Float
-    let starBreakDown: [Int: Int]
-
-    init(totalRatingCount: Int = 0, averageRating: Float = 0, starBreakDown: [Int: Int] = [:]) {
-        self.totalRatingCount = totalRatingCount
-        self.averageRating = averageRating
-        self.starBreakDown = starBreakDown
-    }
-}
-
-struct PopupReviewViewData {
-    let profileImageUrl: String?
-    let nickname: String
-    let rating: Float
-    let reviewDate: String
-    let imagesUrl: [String]?
-    let reviewText: String
-}
-
 final class PopupDetailViewModel: MainCarouselViewModelProtocol {
     private let imageFetchUseCase: ImageFetchUseCaseProtocol
 
     private var carouselPopupImageUrls: [String] = []
-    private var popupMainInformation: PopupMainInformationViewData = PopupMainInformationViewData()
-    private var popupDetailInformation: PopupDetailInformationViewData = PopupDetailInformationViewData()
-    private var popupRating: PopupRatingViewData = PopupRatingViewData()
+    private var popupMainInformation: PopupMainInformationViewData?
+    private var popupDetailInformation: PopupDetailInformationViewData?
+    private var popupRating: PopupRatingViewData?
     private var popupReviews: [PopupReviewViewData] = []
 
     // MARK: - Output
@@ -85,45 +26,11 @@ final class PopupDetailViewModel: MainCarouselViewModelProtocol {
     }
 
     private func bindPopupDetailInformation(_ data: PopupInformation) {
-        let hashTags = data.mainInformation.hashTags ?? []
-
         carouselPopupImageUrls = data.popupImagesUrl
-
-        let startDateString = PopupDateFormatter.formattedPopupStoreDate(from: data.mainInformation.startDate)
-        let endDateString = PopupDateFormatter.formattedPopupStoreDate(from: data.mainInformation.endDate)
-
-        popupMainInformation = PopupMainInformationViewData(
-            popupTitle: data.mainInformation.popupTitle,
-            popupPeriod: "\(startDateString)~\(endDateString)",
-            isUserPick: data.mainInformation.isUserPick,
-            hashTags: hashTags
-        )
-
-        popupDetailInformation = PopupDetailInformationViewData(
-            address: data.detailInformation.address,
-            officialLink: data.detailInformation.officialLink,
-            buisinessHours: data.detailInformation.businesesHours,
-            introduce: data.detailInformation.introduce
-        )
-
-        popupRating = PopupRatingViewData(
-            totalRatingCount: data.totalReview.review.count,
-            averageRating: data.totalReview.averageRating,
-            starBreakDown: data.totalReview.starBreakDown
-        )
-
-        popupReviews = data.totalReview.review.map { review in
-            let profileImageUrl = review.profileImageUrl
-            let reviewImagesUrls = review.reviewImagesUrl
-            return PopupReviewViewData(
-                profileImageUrl: profileImageUrl,
-                nickname: review.nickName,
-                rating: review.reviewRating,
-                reviewDate: PopupDateFormatter.formattedReviewDate(from: review.reviewDate),
-                imagesUrl: reviewImagesUrls,
-                reviewText: review.reviewText
-            )
-        }
+        popupMainInformation = PopupMainInformationViewData(from: data.mainInformation)
+        popupDetailInformation = PopupDetailInformationViewData(from: data.detailInformation)
+        popupRating = PopupRatingViewData(from: data.totalReview)
+        popupReviews = data.totalReview.review.map { PopupReviewViewData(from: $0) }
     }
 
     func fetchImage(url: String, completion: @escaping (Result<Data, ImageFetchError>) -> Void) {
@@ -148,15 +55,18 @@ extension PopupDetailViewModel {
     }
 
     func provideMainInformationData() -> PopupMainInformationViewData {
+        guard let popupMainInformation else { return PopupMainInformationViewData.placeholder }
         return popupMainInformation
     }
 
     func provideDetailInformationData() -> PopupDetailInformationViewData {
+        guard let popupDetailInformation else { return PopupDetailInformationViewData.placeholder }
         return popupDetailInformation
     }
 
+    /// starBreakDown의 value 중 최댓값 반환. 단, value가 같을 경우 key가 가장 큰 원소의 key를 반환
     func provideRatingData() -> (PopupRatingViewData, Int) {
-        // starBreakDown의 value 중 최댓값 반환. 단, value가 같을 경우 key가 가장 큰 원소의 key를 반환
+        guard let popupRating else { return (PopupRatingViewData.placeholder, 0) }
         let maximumIndex = popupRating.starBreakDown.sorted {
             $0.value == $1.value ? $0.key > $1.key : $0.value > $1.value
         }.first?.key ?? 4
@@ -177,5 +87,104 @@ extension PopupDetailViewModel {
 
     func numbersOfCarouselImage() -> Int {
         return carouselPopupImageUrls.count
+    }
+}
+
+// MARK: - View Model
+struct PopupMainInformationViewData {
+    let popupTitle: String
+    let popupPeriod: String
+    let isUserPick: Bool
+    let hashTags: [String]
+
+    static let placeholder = PopupMainInformationViewData(
+        from: PopupMainInformation(
+            popupTitle: "팝콘 팝업스토어",
+            startDate: Date(),
+            endDate: Date(),
+            isUserPick: false,
+            hashTags: []
+        )
+    )
+
+    init(from entity: PopupMainInformation) {
+        let startDateString = PopupDateFormatter.formattedPopupStoreDate(from: entity.startDate)
+        let endDateString = PopupDateFormatter.formattedPopupStoreDate(from: entity.endDate)
+
+        self.popupTitle = entity.popupTitle
+        self.isUserPick = entity.isUserPick
+        self.hashTags = entity.hashTags ?? []
+        self.popupPeriod = "\(startDateString)~\(endDateString)"
+    }
+}
+
+struct PopupDetailInformationViewData {
+    let address: String
+    let officialLink: String
+    let buisinessHours: String
+    let introduce: String
+
+    static let placeholder = PopupDetailInformationViewData(
+        from: PopupDetailInformation(
+            address: "",
+            officialLink: "",
+            businesesHours: "",
+            introduce: ""
+        )
+    )
+
+    init(from entity: PopupDetailInformation) {
+        self.address = entity.address
+        self.officialLink = entity.officialLink
+        self.buisinessHours = entity.businesesHours
+        self.introduce = entity.introduce
+    }
+}
+
+struct PopupRatingViewData {
+    let totalRatingCount: Int
+    let averageRating: Float
+    let starBreakDown: [Int: Int]
+
+    static let placeholder = PopupRatingViewData(
+        from: PopupTotalReview(
+            averageRating: 0,
+            starBreakDown: [0: 0, 1: 0, 2: 0, 3: 0, 4: 0],
+            review: [])
+    )
+
+    init(from entity: PopupTotalReview) {
+        self.totalRatingCount = entity.review.count
+        self.averageRating = entity.averageRating
+        self.starBreakDown = entity.starBreakDown
+    }
+}
+
+struct PopupReviewViewData {
+    let profileImageUrl: String?
+    let nickname: String
+    let rating: Float
+    let reviewDate: String
+    let imagesUrl: [String]?
+    let reviewText: String
+
+    static let placeholder = PopupReviewViewData(
+        from: PopupReview(
+            profileImageUrl: nil,
+            nickName: "사용자",
+            reviewRating: 0,
+            reviewDate: DateFormatter.apiDateFormatter.date(from: "1900-01-01 00:00:00")!,
+            reviewImagesUrl: nil,
+            reviewText: ""
+        )
+    )
+
+    init(from entity: PopupReview) {
+        self.profileImageUrl = entity.profileImageUrl
+        self.nickname = entity.nickName
+        self.rating = entity.reviewRating
+        self.reviewDate = PopupDateFormatter.formattedReviewDate(from: entity.reviewDate)
+        self.imagesUrl = entity.reviewImagesUrl
+        self.reviewText = entity.reviewText
     }
 }
