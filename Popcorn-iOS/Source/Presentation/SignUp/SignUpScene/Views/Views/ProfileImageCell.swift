bcfe4bbbacb8936ec09bbc5afccc33b3dc2270e7
snughnu <ksnughnu@gmail.com>
2025-02-16 14:24:39 +0900

    refactor: 프로필이미지 에셋 변경으로 인한 코드변경

--- Popcorn-iOS/Source/Presentation/SignUp/SignUpScene/Views/Views/ProfileImageCell.swift
+++ Popcorn-iOS/Source/Presentation/SignUp/SignUpScene/Views/Views/ProfileImageCell.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ProfileImageCell: UICollectionViewCell {
-    private let cellBackgroundView = UIView()
     private let imageView = UIImageView()
     private let cellSize = (UIScreen.main.bounds.width - (29 * 2 + 18 * 2)) / 3
 
@@ -21,41 +20,30 @@ class ProfileImageCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
-    func configureContents(image: UIImage, color: UIColor, isSelected: Bool) {
+// MARK: - Public interface
+extension ProfileImageCell {
+    func configureContents(image: UIImage) {
         imageView.image = image
-        cellBackgroundView.backgroundColor = color
-        cellBackgroundView.layer.borderWidth = isSelected ? 3 : 0
-        cellBackgroundView.layer.borderColor = isSelected ? UIColor(resource: .popcornOrange).cgColor : nil
     }
 }
 
 // MARK: - Configure UI
 extension ProfileImageCell {
     private func configureSubviews() {
-        cellBackgroundView.layer.cornerRadius = cellSize / 2
-        cellBackgroundView.clipsToBounds = true
-
         imageView.layer.cornerRadius = cellSize / 2
         imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
-
-        contentView.addSubview(cellBackgroundView)
         contentView.addSubview(imageView)
     }
 
     private func configureLayout() {
-        cellBackgroundView.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            cellBackgroundView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            cellBackgroundView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            cellBackgroundView.widthAnchor.constraint(equalToConstant: cellSize),
-            cellBackgroundView.heightAnchor.constraint(equalToConstant: cellSize),
-
-            imageView.centerXAnchor.constraint(equalTo: cellBackgroundView.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: cellBackgroundView.centerYAnchor),
+            imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             imageView.widthAnchor.constraint(equalToConstant: cellSize),
             imageView.heightAnchor.constraint(equalToConstant: cellSize)
         ])
