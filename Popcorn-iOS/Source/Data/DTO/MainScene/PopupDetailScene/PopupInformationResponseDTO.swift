694b78eb17ed91aec31dee3bfd24578942234650
MinwooJe <gpatration77@gmail.com>
2025-02-17 16:46:04 +0900

    feat: 상세화면 정보탭 DTO 구현

--- Popcorn-iOS/Source/Data/DTO/MainScene/PopupDetailScene/PopupInformationResponseDTO.swift
+++ Popcorn-iOS/Source/Data/DTO/MainScene/PopupDetailScene/PopupInformationResponseDTO.swift
@@ -5,3 +5,36 @@
 //  Created by 제민우 on 2/17/25.
 //
 
+import Foundation
+
+struct PopupInformationResponseDTO: Decodable {
+    let popupId: Int
+    let popupImages: [String]
+    let popupTitle: String
+    let startDate: String
+    let endDate: String
+    let isUserPick: Bool
+    let hashTags: [String]
+
+    let address: String
+    let officialLink: String
+    let businesesHours: String
+    let introduce: String
+    let reservationUrl: String
+
+    enum CodingKeys: String, CodingKey {
+        case popupId
+        case popupImages = "popupImage"
+        case popupTitle = "title"
+        case startDate = "startedAt"
+        case endDate = "endedAt"
+        case isUserPick = "isLiked"
+        case hashTags
+
+        case address = "location"
+        case officialLink = "organizerUrl"
+        case businesesHours = "hours"
+        case introduce = "contents"
+        case reservationUrl
+    }
+}
