2cba851ab5cde8b2b36bbed4d9552b317ee41a4b
MinwooJe <gpatration77@gmail.com>
2025-02-20 17:06:56 +0900

    feat: PopupDetailUseCaseProtocol 구현

---
+++ Popcorn-iOS/Source/Domain/UseCases/MainScene/Interfaces/PopupDetailUseCaseProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  PopupDetailUseCaseProtocol.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 2/20/25.
+//
+
+protocol PopupDetailUseCaseProtocol {
+    func fetchPopupAllData(completion: @escaping (
+        Result<(PopupInformation, PopupRatingDistribution, PopupReviewList), Error>) -> Void
+    )
+
+    func fetchPopupReviews(completion: @escaping (Result<PopupReviewList, Error>) -> Void)
+
+    func extractHashTag(from popupInformation: PopupInformation) -> [String]
+}
