54dfe893344374a8ee3ad0ee3d0f8d6a7b2ffef5
MinwooJe <gpatration77@gmail.com>
2025-02-24 16:55:12 +0900

    test: 상세화면 유즈케이스 해시태그 추출 테스트 코드 작성

---
+++ Popcorn-iOSTests/TestDouble/Dummy/DummyPopupDetailRepository.swift
@@ -0,0 +1,25 @@
+//
+//  DummyPopupDetailRepository.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 2/24/25.
+//
+
+@testable import Popcorn_iOS
+
+final class DummyPopupDetailRepository: PopupDetailRepositoryProtocol {
+    func fetchPopupAllData(popupId: Int, completion: @escaping (
+        Result<(Popcorn_iOS.PopupInformation, Popcorn_iOS.PopupRatingDistribution,
+                Popcorn_iOS.PopupReviewList), any Error>) -> Void
+    ) {
+        completion(.failure(NetworkError.emptyData))
+    }
+    
+    func fetchPopupReviews(
+        popupId: Int,
+        page: Int,
+        completion: @escaping (Result<Popcorn_iOS.PopupReviewList, any Error>) -> Void
+    ) {
+        completion(.failure(NetworkError.emptyData))
+    }
+}
