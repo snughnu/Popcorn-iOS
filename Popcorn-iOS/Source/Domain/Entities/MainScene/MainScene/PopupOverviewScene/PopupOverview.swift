7416901a5bd1efddfb93bab2f35e2ab2041b8bfe
MinwooJe <gpatration77@gmail.com>
2025-03-12 15:04:48 +0900

    feat: PopupOverview 엔티티 구현

---
+++ Popcorn-iOS/Source/Domain/Entities/MainScene/MainScene/PopupOverviewScene/PopupOverview.swift
@@ -0,0 +1,16 @@
+//
+//  PopupOverview.swift
+//  Popcorn-iOS
+//
+//  Created by 제민우 on 3/12/25.
+//
+
+import Foundation
+
+struct PopupOverview {
+    let popupImageUrl: String
+    let popupTitle: String
+    let startDate: Date
+    let endDate: Date
+    let address: String
+}
