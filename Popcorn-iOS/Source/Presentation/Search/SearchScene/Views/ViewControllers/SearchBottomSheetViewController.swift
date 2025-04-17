96258e435eea535329e3cd335b88ff4d2569cb99
snughnu <ksnughnu@gmail.com>
2025-04-17 13:30:01 +0900

    feat: 바텀시트 뷰, 뷰컨 작성

--- Popcorn-iOS/Source/Presentation/Search/SearchScene/Views/ViewControllers/SearchBottomSheetViewController.swift
+++ Popcorn-iOS/Source/Presentation/Search/SearchScene/Views/ViewControllers/SearchBottomSheetViewController.swift
@@ -8,22 +8,14 @@
 import UIKit
 
 class SearchBottomSheetViewController: UIViewController {
+    // MARK: - Properties
+    private let searchBottomSheetView = SearchBottomSheetView()
+
+    override func loadView() {
+        view = searchBottomSheetView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Popcorn-iOS/Source/Presentation/Search/SearchScene/Views/Views/SearchBottomSheetView.swift
+++ Popcorn-iOS/Source/Presentation/Search/SearchScene/Views/Views/SearchBottomSheetView.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 class SearchBottomSheetView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureInitialSetting()
+    }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Configure Initial Setting
+extension SearchBottomSheetView {
+    private func configureInitialSetting() {
+        backgroundColor = .green
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+    }
 }
