cb115d8deec03279376e03034d6719d878f6f731
snughnu <ksnughnu@gmail.com>
2025-04-24 14:08:43 +0900

    feat: 바텀시트 - 위치 버튼 UI 추가

--- Popcorn-iOS/Source/Presentation/Search/SearchScene/Views/Views/SearchBottomSheetView.swift
+++ Popcorn-iOS/Source/Presentation/Search/SearchScene/Views/Views/SearchBottomSheetView.swift
@@ -8,9 +8,36 @@
 import UIKit
 
 class SearchBottomSheetView: UIView {
+    private lazy var locationButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "내 위치는 여기"
+        config.image = UIImage(resource: .location)
+        config.baseBackgroundColor = .white
+        config.baseForegroundColor = UIColor(.black)
+        config.imagePadding = 4
+        config.contentInsets = NSDirectionalEdgeInsets(top: 7, leading: 14, bottom: 7, trailing: 14)
+        config.cornerStyle = .capsule
+
+        let button = UIButton(configuration: config, primaryAction: nil)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor(resource: .popcornGray1).cgColor
+        button.clipsToBounds = true
+        let height = button.intrinsicContentSize.height
+        button.layer.cornerRadius = height / 2
+
+        if let baseFont = UIFont(name: RobotoFontName.robotoMedium, size: 15) {
+            let scaledFont = UIFontMetrics(forTextStyle: .body).scaledFont(for: baseFont)
+            button.titleLabel?.font = scaledFont
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+        }
+        return button
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureInitialSetting()
+        configureSubviews()
+        configureLayout()
     }
 
     required init?(coder: NSCoder) {
@@ -24,5 +51,24 @@ extension SearchBottomSheetView {
         backgroundColor = UIColor(.white)
         layer.cornerRadius = 10
         layer.masksToBounds = true
+    }
+}
+
+// MARK: - Configure AutoLayout
+extension SearchBottomSheetView {
+    private func configureSubviews() {
+        [
+            locationButton
+        ].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            locationButton.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+            locationButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 43)
+        ])
     }
 }
